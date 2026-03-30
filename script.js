0804f1a42d2900f4dd9fb7e28e144d73a30f2357
rancgenevois <rancgenevois@gmail.com>
2026-03-30 19:18:08 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+const menuToggle = document.querySelector(".menu-toggle");
+const mainNav = document.querySelector(".main-nav");
+
+if (menuToggle && mainNav) {
+  menuToggle.addEventListener("click", () => {
+    mainNav.classList.toggle("open");
+  });
+}
